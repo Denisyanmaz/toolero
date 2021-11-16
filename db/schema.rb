0e4e7498398a2d69933e42e93d68cc4a6406913f
Denis Yanmaz <denis.yanmaz.dy@gmail.com>
2021-11-16 15:42:30 +0100

    seeds and user uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_103428) do
+ActiveRecord::Schema.define(version: 2021_11_16_142259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_103428) do
   create_table "tools", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "type"
+    t.string "tool_type"
     t.integer "price"
     t.boolean "availability"
     t.bigint "user_id", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.new(
+    username: Faker::Internet.username,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  user.save!
+  (1..5).to_a.sample.times do
+    tool = Tool.new(
+      name: Faker::Name.first_name,
+      tool_type: Faker::Name.middle_name,
+      price: Faker::Number.within(range: 1..15),
+      description: Faker::Lorem.sentence(word_count: 6),
+      availability: true,
+      user: user
+    )
+    tool.save!
+  end
+end
