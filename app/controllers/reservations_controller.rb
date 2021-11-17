17775e6a3a66bfb403cbed5cc16c069b77fd0d05
Anna Waldorf <anna@waldorf.de>
2021-11-17 12:12:17 +0100

    added the reservation controller & reservation new & show action

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,24 @@
+class ReservationsController < ApplicationController
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def create
+    @tool = Tool.find(params[:tool_id])
+    @reservation = Reservation.new(strong_params)
+    @reservation.user = current_user
+    @reservation.tool = @tool
+    if @reservation.save
+      redirect_to reservation_path(@reservation)
+    else
+      render "/tools/show"
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
+
+end
